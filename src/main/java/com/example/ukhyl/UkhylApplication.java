a0ecdda207e3cf62de07b9e3bdc365b29aefcbff
ArtemLyah <asuskonotopo@gmail.com>
2024-10-01 18:17:15 +0300

    init

---
+++ src/main/java/com/example/ukhyl/UkhylApplication.java
@@ -0,0 +1,13 @@
+package com.example.ukhyl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkhylApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(UkhylApplication.class, args);
+  }
+  
+}
---
+++ src/test/java/com/example/ukhyl/UkhylApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ukhyl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkhylApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
